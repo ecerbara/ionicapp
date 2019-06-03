4fb947e71d4f916c90d9fca2f56bee2d7c717fc3
EMEA\cerbara <emiliano.cerbara@dxc.com>
2019-06-03 22:53:17 +0200

    #0 fix spring boot class

--- test-client/src/main/java/it/mef/common/test/TestClientApplication.java
+++ test-client/src/main/java/it/mef/common/test/TestClientApplication.java
@@ -2,14 +2,21 @@ package it.mef.common.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class TestClientApplication {
+public class TestClientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestClientApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TestClientApplication.class);
+    }
 }
