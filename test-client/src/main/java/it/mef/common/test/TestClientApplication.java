9098310115450ba5f78ddf7ced75fd6e5efc3ac6
EMEA\cerbara <emiliano.cerbara@dxc.com>
2019-06-03 22:15:12 +0200

    #0 fix spring boot for openshift

--- test-client/src/main/java/it/mef/common/test/TestClientApplication.java
+++ test-client/src/main/java/it/mef/common/test/TestClientApplication.java
@@ -2,12 +2,10 @@ package it.mef.common.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"it.mef.gestionemessaggi.client"})
 @EnableSwagger2
 public class TestClientApplication {
 
